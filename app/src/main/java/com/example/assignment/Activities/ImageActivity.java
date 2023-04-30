f69607f884ec1f4b76e9d7347d10d4f2f94f40fd
Durgesh Pawar <durgeshhulkiron@gmail.com>
2023-04-30 20:38:39 +0530

    commit

--- app/src/main/java/com/example/assignment/Activities/ImageActivity.java
+++ app/src/main/java/com/example/assignment/Activities/ImageActivity.java
@@ -5,20 +5,28 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import com.example.assignment.MainActivity;
 import com.example.assignment.R;
 
 public class ImageActivity extends AppCompatActivity {
     Button backBtn, AddImageBtn;
+    ImageView selectedImage;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image);
 
         backBtn = findViewById(R.id.backBtn);
+        AddImageBtn = findViewById(R.id.addImage);
+        selectedImage = findViewById(R.id.displayImage);
+
         Intent toMainActivity = new Intent(getApplicationContext(), MainActivity.class);
 
         backBtn.setOnClickListener(v -> startActivity(toMainActivity));
+
+
     }
 }
