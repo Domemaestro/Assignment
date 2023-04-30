5c724eff69d106170b10e1862907782dc746c628
Durgesh Pawar <durgeshhulkiron@gmail.com>
2023-04-30 18:30:37 +0530

    commit

--- app/src/main/java/com/example/assignment/MainActivity.java
+++ app/src/main/java/com/example/assignment/MainActivity.java
@@ -2,10 +2,13 @@ package com.example.assignment;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
-
 import android.widget.Button;
 import android.widget.Toast;
+
+import com.example.assignment.Activities.ImageActivity;
+import com.example.assignment.Activities.MusicActivity;
 
 public class MainActivity extends AppCompatActivity {
     Button firstActivity,secondActivity;
@@ -16,14 +19,18 @@ public class MainActivity extends AppCompatActivity {
 
         firstActivity = findViewById(R.id.button1);
         secondActivity = findViewById(R.id.button2);
+        Intent toMusicActivity = new Intent(getApplicationContext(), MusicActivity.class);
+        Intent toImageActivity = new Intent(getApplicationContext(), ImageActivity.class);
 
-        firstActivity.setOnClickListener(v ->
+        firstActivity.setOnClickListener(v -> {
+            startActivity(toMusicActivity);
+            Toast.makeText(getApplicationContext(),"First Activity",Toast.LENGTH_SHORT).show();
+        });
 
-                Toast.makeText(getApplicationContext(),"First Activity",Toast.LENGTH_SHORT).show()
-        );
-
-        secondActivity.setOnClickListener(v ->
-                Toast.makeText(getApplicationContext(),"Second Activity",Toast.LENGTH_SHORT).show()
+        secondActivity.setOnClickListener(v -> {
+            startActivity(toImageActivity);
+            Toast.makeText(getApplicationContext(),"Second Activity",Toast.LENGTH_SHORT).show();
+                }
         );
     }
 }
