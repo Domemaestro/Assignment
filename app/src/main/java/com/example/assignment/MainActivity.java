f7fb5725ec981f4ce0eeb62bb5c798a69cd922da
Durgesh Pawar <durgeshhulkiron@gmail.com>
2023-04-30 17:57:32 +0530

    commit

--- app/src/main/java/com/example/assignment/MainActivity.java
+++ app/src/main/java/com/example/assignment/MainActivity.java
@@ -4,11 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import android.widget.Button;
+import android.widget.Toast;
 
+public class MainActivity extends AppCompatActivity {
+    Button firstActivity,secondActivity;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        firstActivity = findViewById(R.id.button1);
+        secondActivity = findViewById(R.id.button2);
+
+        firstActivity.setOnClickListener(v -> Toast.makeText(getApplicationContext(),"First Activity",Toast.LENGTH_SHORT).show());
+
+        secondActivity.setOnClickListener(v -> Toast.makeText(getApplicationContext(),"Second Activity",Toast.LENGTH_SHORT).show());
     }
 }
