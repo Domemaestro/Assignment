4bc1c2f226e3234fea7838bab7c058f1c2850d59
Durgesh Pawar <durgeshhulkiron@gmail.com>
2023-04-30 19:06:34 +0530

    commit

--- app/src/main/java/com/example/assignment/Activities/ImageActivity.java
+++ app/src/main/java/com/example/assignment/Activities/ImageActivity.java
@@ -2,15 +2,23 @@ package com.example.assignment.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
+import com.example.assignment.MainActivity;
 import com.example.assignment.R;
 
 public class ImageActivity extends AppCompatActivity {
-
+    Button backBtn, AddImageBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image);
+
+        backBtn = findViewById(R.id.backBtn);
+        Intent toMainActivity = new Intent(getApplicationContext(), MainActivity.class);
+
+        backBtn.setOnClickListener(v -> startActivity(toMainActivity));
     }
 }
--- app/src/main/java/com/example/assignment/Activities/MusicActivity.java
+++ app/src/main/java/com/example/assignment/Activities/MusicActivity.java
@@ -2,15 +2,24 @@ package com.example.assignment.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
+import com.example.assignment.MainActivity;
 import com.example.assignment.R;
 
 public class MusicActivity extends AppCompatActivity {
 
+    Button backBtn, playBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_music);
+
+        backBtn = findViewById(R.id.backBtn);
+        Intent toMainActivity = new Intent(getApplicationContext(), MainActivity.class);
+
+        backBtn.setOnClickListener(v -> startActivity(toMainActivity));
     }
 }
