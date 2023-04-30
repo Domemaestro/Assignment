3736c46927a01aec8c1d6b7cd97e1ba543f3a2d9
Durgesh Pawar <durgeshhulkiron@gmail.com>
2023-04-30 21:57:44 +0530

    commit

--- app/src/main/java/com/example/assignment/Activities/ImageActivity.java
+++ app/src/main/java/com/example/assignment/Activities/ImageActivity.java
@@ -1,9 +1,12 @@
 package com.example.assignment.Activities;
 
+import androidx.activity.result.ActivityResultLauncher;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.widget.Button;
 import android.widget.ImageView;
 
@@ -12,7 +15,8 @@ import com.example.assignment.R;
 
 public class ImageActivity extends AppCompatActivity {
     Button backBtn, AddImageBtn;
-    ImageView selectedImage;
+    ImageView dispImage;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,12 +25,35 @@ public class ImageActivity extends AppCompatActivity {
 
         backBtn = findViewById(R.id.backBtn);
         AddImageBtn = findViewById(R.id.addImage);
-        selectedImage = findViewById(R.id.displayImage);
+        dispImage = findViewById(R.id.displayImage);
 
         Intent toMainActivity = new Intent(getApplicationContext(), MainActivity.class);
 
         backBtn.setOnClickListener(v -> startActivity(toMainActivity));
 
-
+        AddImageBtn.setOnClickListener(v -> selectImageFromStorage());
     }
+
+    private void selectImageFromStorage(){
+        Intent selectImage = new Intent();
+        selectImage.setType("image/*");
+        selectImage.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(selectImage,"Select Image"),1);
+    }
+
+     @Override
+     public void onActivityResult(int requestCode, int resultCode, Intent data) {
+         super.onActivityResult(requestCode, resultCode, data);
+
+         if (resultCode == RESULT_OK){
+             if (requestCode == 1){
+                 Uri imageUri = data.getData();
+                 if(null != imageUri){
+                     dispImage.setImageURI(imageUri);
+                 }
+             }
+         }
+     }
+
+
 }
