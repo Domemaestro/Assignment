1e5af70edb29e432319bb1f2842f742dc2276fed
Durgesh Pawar <durgeshhulkiron@gmail.com>
2023-05-01 09:41:12 +0530

    commit

--- app/src/main/java/com/example/assignment/Activities/MusicActivity.java
+++ app/src/main/java/com/example/assignment/Activities/MusicActivity.java
@@ -4,17 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
-import android.provider.Settings;
-import android.view.View;
 import android.widget.Button;
 
 import com.example.assignment.MainActivity;
 import com.example.assignment.R;
 
+import java.io.IOException;
+
 public class MusicActivity extends AppCompatActivity {
 
-    Button backBtn, playBtn;
+    Button backBtn, pickBtn;
     MediaPlayer musicPlayer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,19 +23,40 @@ public class MusicActivity extends AppCompatActivity {
         setContentView(R.layout.activity_music);
 
         backBtn = findViewById(R.id.backBtn);
-        playBtn = findViewById(R.id.playBtn);
-        musicPlayer = MediaPlayer.create(this, Settings.System.DEFAULT_RINGTONE_URI);
-
+        pickBtn = findViewById(R.id.pickBtn);
+//        musicPlayer = MediaPlayer.create(this, Settings.System.DEFAULT_RINGTONE_URI);
 
         Intent toMainActivity = new Intent(getApplicationContext(), MainActivity.class);
 
         backBtn.setOnClickListener(v -> startActivity(toMainActivity));
 
-        playBtn.setOnClickListener(v -> {
-           musicPlayer.start();
-           musicPlayer.setLooping(true);
+        pickBtn.setOnClickListener(v -> {
+            Intent pickAudio = new Intent(Intent.ACTION_GET_CONTENT);
+            pickAudio.setType("audio/*");
+            startActivityForResult(pickAudio,100);
         });
 
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 100){
+            if (data!=null){
+                Uri audioUri = data.getData();
+                buildMediaPlayer(audioUri);
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    public void buildMediaPlayer(Uri uri){
+        musicPlayer = new MediaPlayer();
+        try {
+            musicPlayer.setDataSource(getApplicationContext(),uri);
+            musicPlayer.prepare();
+            musicPlayer.start();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 
