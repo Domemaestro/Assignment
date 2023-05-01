40a64186b36eaeb2a26e29d5acef386a520ea0a8
Durgesh Pawar <durgeshhulkiron@gmail.com>
2023-05-01 09:05:41 +0530

    commit

--- app/src/main/java/com/example/assignment/Activities/MusicActivity.java
+++ app/src/main/java/com/example/assignment/Activities/MusicActivity.java
@@ -3,7 +3,10 @@ package com.example.assignment.Activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.view.View;
 import android.widget.Button;
 
 import com.example.assignment.MainActivity;
@@ -12,14 +15,28 @@ import com.example.assignment.R;
 public class MusicActivity extends AppCompatActivity {
 
     Button backBtn, playBtn;
+    MediaPlayer musicPlayer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_music);
 
         backBtn = findViewById(R.id.backBtn);
+        playBtn = findViewById(R.id.playBtn);
+        musicPlayer = MediaPlayer.create(this, Settings.System.DEFAULT_RINGTONE_URI);
+
+
         Intent toMainActivity = new Intent(getApplicationContext(), MainActivity.class);
 
         backBtn.setOnClickListener(v -> startActivity(toMainActivity));
+
+        playBtn.setOnClickListener(v -> {
+           musicPlayer.start();
+           musicPlayer.setLooping(true);
+        });
+
     }
+
+
+
 }
